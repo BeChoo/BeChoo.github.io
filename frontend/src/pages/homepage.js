57ff7b819b1f16c28b166dc306bad1b995b5baaf
Brian Bui <78934614+huybui0424@users.noreply.github.com>
2024-02-17 14:45:27 -0800

    Update

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,25 +1,31 @@
 // Filename - pages/homepage.js
 
 import React from 'react';
-import './homepage.css';
-import MapContainer from './map';
+import './homepage.css'; // Importing CSS for styling
+import MapContainer from './map'; // Importing MapContainer component
 
+// Functional component Homepage
 const Homepage = () => {
   return (
     <div>
+      {/* Header section */}
       <header>
         <h1>This is our homepage. Begin browsing hotels below!</h1>
       </header>
 
+      {/* Main section */}
       <main>
+        {/* Search bar */}
         <form className="searchBar ">
           <input type="search" />
         </form>
 
+        {/* Map container */}
         <div className="map-container center">
-        <MapContainer />
+          <MapContainer /> {/* Rendering MapContainer component */}
         </div>
 
+        {/* Row section for previous and next hotel */}
         <div className="row ">
           <div className="column">
             <p>Previous Hotel <i className="arrow left"></i></p>
@@ -32,4 +38,4 @@ const Homepage = () => {
   );
 };
 
-export default Homepage;
+export default Homepage; // Exporting Homepage component
--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -3,12 +3,14 @@ import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 class MapContainer extends Component {
   render() {
+    // Define the styles for the map container
     const mapStyles = {
-      width: '500px', // Adjust the width as needed
-      height: '500px', // Adjust the height as needed
-      display: 'flex'
+      width: '500px', // Adjust the width 
+      height: '500px', // Adjust the height 
+      display: 'flex' // Using flex to control layout
     };
     return (
+      // Render the Map component provided by google-maps-react
       <Map
         google={this.props.google}
         zoom={15}
@@ -22,6 +24,7 @@ class MapContainer extends Component {
   }
 }
 
+// Export the MapContainer component wrapped with GoogleApiWrapper
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBpcnohG7JvM8ZjcMmwN8ej0QG6wnb-TLI' // Replace 'YOUR_API_KEY' with your actual API key
+  apiKey: 'AIzaSyBpcnohG7JvM8ZjcMmwN8ej0QG6wnb-TLI' 
 })(MapContainer);
