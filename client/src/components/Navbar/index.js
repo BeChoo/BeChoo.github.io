5a2b386fd9c581793314509bdb57b33e167a4f4d
BeChoo <bcho2014@gmail.com>
2024-02-20 15:25:26 -0800

    Added files

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,29 +1,29 @@
 // Filename - "./components/Navbar.js
- 
+
 import React from "react";
 import { Nav, NavLink, NavMenu } from "./NavbarElements";
- 
+
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavMenu>
-                    <NavLink to="/about" activeStyle>
-                        About
-                    </NavLink>
-                    <NavLink to="/contact" activeStyle>
-                        Contact Us
-                    </NavLink>
                     <NavLink to="/homepage" activeStyle>
                         Home
                     </NavLink>
                     <NavLink to="/sign-up" activeStyle>
                         Sign Up
                     </NavLink>
+                    <NavLink to="/about" activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to="/contact" activeStyle>
+                        Contact Us
+                    </NavLink>
                 </NavMenu>
             </Nav>
         </>
     );
 };
- 
+
 export default Navbar;
