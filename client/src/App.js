6906bced2a55fc04fce060a95c7f0c1715379139
chascc <Chase.Calero@student.csulb.edu>
2024-03-09 22:20:56 -0800

    Implements new signup page at localhost:3000/

--- client/src/App.js
+++ client/src/App.js
@@ -9,23 +9,24 @@ import './App.css';
 import Home from "./pages";
 import About from "./pages/about";
 import Homepage from "./pages/homepage";
-import SignUp from "./pages/signup";
+import SignOn from "./pages/signup";
 import Compare from "./pages/compare";
 import Mappage from "./pages/mappage";
 import HotelDetailPage from "./pages/hoteldetails";
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
  
 function App() {
     return (
         <Router>
-            <Navbar />
             <Routes>
-                <Route exact path="/" element={<Home />} />
+                <Route exact path="/" element={<Signup />} />
+                <Route path='/login' element={<Login />} />
                 <Route path="/index" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/homepage" element={<Homepage />} />
                 <Route path="/mappage" element={<Mappage />} />
                 <Route path="/compare" element={<Compare />} />
-                <Route path="/sign-up" element={<SignUp />} />
                 <Route path="/hotel/:id" element={<HotelDetailPage />} />
             </Routes>
         </Router>
