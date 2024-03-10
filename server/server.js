f8fe271943566f03de16157081ca12fc154a8e7f
chascc <Chase.Calero@student.csulb.edu>
2024-03-09 22:27:10 -0800

    Connects to mongodb and checks for user validity

--- server/server.js
+++ server/server.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const cors = require("cors")
+const EmployeeModel = require('./models/Employee')
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json())
+app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+mongoose.connect("mongodb+srv://chasecalero:chasecalero@gotel.pkl54mr.mongodb.net/Gotel?retryWrites=true&w=majority")
+.then(() =>{
+  console.log("mongodb connected");
+})
+.catch(() =>{
+  console.log("connection failed");
+})
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.post("/login", (req, res) => {
+  const {email, password} = req.body
+  EmployeeModel.findOne({email: email})
+  .then(user => {
+    if (user){
+        if(user.password === password){
+          res.json("Success")
+        }else{
+          res.json("The password is incorrect")
+        }
+    }else{
+      res.json("No record exists")
+    }
+  })
+})
+
+app.post('/register', (req, res) => {
+  EmployeeModel.create(req.body)
+  .then(employees => res.json(employees))
+  .catch(err => res.json(err))
+})
+
+//const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(3001, () => {
+  console.log(`Server is running`);
 });
