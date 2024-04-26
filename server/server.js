469ecbf1b0e8771630b915e096dbb89fea1694b5
chascc <Chase.Calero@student.csulb.edu>
2024-04-26 06:00:31 -0700

    Changed port number to 3002

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors");
-const CityModel = require('../client/src/pages/api/city');
-const hotelModel = require('../client/src/pages/api/hotels');
 const EmployeeModel = require('./models/User')
 
 const app = express();
@@ -17,36 +15,6 @@ mongoose.connect("mongodb+srv://chasecalero:chasecalero@gotel.pkl54mr.mongodb.ne
     console.log("Connection failed");
   });
 
-// Route for fetching city data
-app.get("/api/city", (req, res) => {
-  const { searchCity } = req.query;
-  CityModel.findOne({ name: searchCity })
-    .then(city => {
-      if (city) {
-        res.json(city);
-      } else {
-        res.status(404).json({ message: "City not found" });
-      }
-    })
-    .catch(error => {
-      console.error("Error fetching city data:", error);
-      res.status(500).json({ message: "Internal server error" });
-    });
-});
-
-// Route for fetching hotel data
-app.get("/api/hotels", (req, res) => {
-  const { city, checkIn, checkOut, guests } = req.query;
-  // Assuming you have a model function to fetch hotels based on the provided parameters
-  hotelModel.find({ city, checkIn, checkOut, guests })
-      .then((hotels) => {
-          res.json(hotels);
-      })
-      .catch((err) => {
-          console.error("Error fetching hotel data:", err);
-          res.status(500).json({ error: "Internal server error" });
-      });
-});
 
 app.post("/login", (req, res) => {
   const {email, password} = req.body
@@ -71,8 +39,6 @@ app.post('/register', (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3002, () => {
+  console.log(`Server is running`);
 });
