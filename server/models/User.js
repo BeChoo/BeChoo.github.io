a44ae2e4a4ec0e8564bdc02ddbb0f8ca06d554b5
chascc <Chase.Calero@student.csulb.edu>
2024-04-30 21:48:05 -0700

    Updated schema to include reviews

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const EmployeeSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
+// Define the schema for individual reviews
+const reviewSchema = new mongoose.Schema({
+    hotelId: mongoose.Types.ObjectId, // ID of the hotel reviewed
+    rating: Number, // Rating given by the user
+    reviewText: String // Textual review provided by the user
+  });
 
-const EmployeeModel = mongoose.model("employees", EmployeeSchema)
-module.exports = EmployeeModel
+  // Define the schema for the user
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  reviews: [reviewSchema], // Array of reviews given by the user
+});
+
+// Create models from the defined schemas
+const UserModel = mongoose.model("User", UserSchema);
+const ReviewModel = mongoose.model("Review", reviewSchema);
+
+// Export the models
+module.exports = { UserModel, ReviewModel };
