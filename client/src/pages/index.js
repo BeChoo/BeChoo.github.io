9070cdfd64e4bb64f4ef14635592ff9e2ee64657
chascc <Chase.Calero@student.csulb.edu>
2024-04-30 21:46:10 -0700

    Hard code for Hotel of the Day

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,6 +5,7 @@ import QueenMary from "../images/Queen Mary.png"; // Adjusted import path
 import Renaissance from "../images/Renaissance.png"; // Adjusted import path
 import Residence from "../images/Residence Inn.png"; // Adjusted import path
 
+// Home component rendering hotel images and information
 const Home = () => {
     return (
         <div>
@@ -14,6 +15,15 @@ const Home = () => {
                 <div className="image-container"><img src={QueenMary} alt="QueenMary"/></div>
                 <div className="image-container"><img src={Renaissance} alt="Renaissance"/></div>
                 <div className="image-container"><img src={Residence} alt="Residence"/></div>
+            </div>
+            <div style={{textAlign: "center", marginTop: "50px"}}>
+                <h2>Hotel of the Day!</h2>
+                <h3>Kawada Hotel</h3>
+                <div style={{textAlign: "center"}}>
+                    <img src="https://images.trvl-media.com/lodging/1000000/30000/25200/25151/9eda9964.jpg?impolicy=resizecrop&rw=1200&ra=fit" alt="Kawada Hotel" style={{width: "300px", height: "200px"}} />
+                </div>
+                <p style={{fontSize: "12px", marginTop: "10px"}}>Based on your location and limited time offers</p>
+                <p style={{fontSize: "20px", marginTop: "10px"}}>★★★</p>
             </div>
         </div>
     );
