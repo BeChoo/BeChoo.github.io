e68fa234a3c8097b7b45affc3adbdb0c9f02812c
BeChoo <bcho2014@gmail.com>
2024-05-11 12:44:13 -0700

    Updated corsOptions in server.js

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const { UserModel, ReviewModel } = require('./models/User');
 
 const app = express();
-app.use(express.json());
 const corsOptions = {
-  origin: 'https://gotel-frontend.vercel.app', // This should be your frontend URL
-  optionsSuccessStatus: 200
+  origin: ['https://gotel-frontend.vercel.app'],
+  METHODS: ["POST", "GET"],
+  credentials: true
+  // optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
+app.use(express.json());
 
 
 mongoose.connect("mongodb+srv://chasecalero:chasecalero@gotel.pkl54mr.mongodb.net/Gotel?retryWrites=true&w=majority")
@@ -21,27 +23,27 @@ mongoose.connect("mongodb+srv://chasecalero:chasecalero@gotel.pkl54mr.mongodb.ne
   });
 
 
-  app.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    UserModel.findOne({ email: email })
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email: email })
     .then(user => {
-        if (user && user.password === password) {
-            res.json({ message: "Success", user });
-        } else {
-            res.status(401).json({ message: "Invalid credentials" });
-        }
+      if (user && user.password === password) {
+        res.json({ message: "Success", user });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
     })
     .catch(err => {
-        console.error("Login error:", err);
-        res.status(500).json({ error: "Error logging in", details: err.toString() });
+      console.error("Login error:", err);
+      res.status(500).json({ error: "Error logging in", details: err.toString() });
     });
 });
 
 
 app.post('/register', (req, res) => {
   UserModel.create(req.body)
-  .then(employees => res.json(employees))
-  .catch(err => res.json(err))
+    .then(employees => res.json(employees))
+    .catch(err => res.json(err))
 })
 
 // Submit a review
@@ -51,18 +53,18 @@ app.post('/submitReview', (req, res) => {
   console.log("Review details:", { hotelId, hotelName, hotelImageURL, rating, reviewText });
 
   UserModel.findByIdAndUpdate(
-      userId,
-      { $push: { reviews: { hotelId, hotelName, hotelImageURL, rating, reviewText } } },
-      { new: true, safe: true, upsert: true }
+    userId,
+    { $push: { reviews: { hotelId, hotelName, hotelImageURL, rating, reviewText } } },
+    { new: true, safe: true, upsert: true }
   )
-  .then(user => {
+    .then(user => {
       console.log("Review added successfully:", user);
       res.status(200).json({ review: { hotelId, hotelName, hotelImageURL, rating, reviewText } });
-  })
-  .catch(err => {
+    })
+    .catch(err => {
       console.error("Error submitting review:", err);
       res.status(500).json({ error: "Error submitting review", details: err });
-  });
+    });
 });
 
 
@@ -74,31 +76,31 @@ app.get('/userReviews/:userId', (req, res) => {
   const userId = req.params.userId;
   //console.log("Fetching reviews for user ID:", userId);
   UserModel.findById(userId)
-  .populate('reviews')  // Ensure 'reviews' refers to a valid path if it’s supposed to be a populated field
-  .then(user => {
+    .populate('reviews')  // Ensure 'reviews' refers to a valid path if it’s supposed to be a populated field
+    .then(user => {
       if (user && user.reviews) {
-          //console.log("Reviews found:", user.reviews);
-          res.status(200).json(user.reviews);
+        //console.log("Reviews found:", user.reviews);
+        res.status(200).json(user.reviews);
       } else {
-          //console.log("No user or reviews found for ID:", userId);
-          res.status(404).json({ message: "User not found or no reviews" });
+        //console.log("No user or reviews found for ID:", userId);
+        res.status(404).json({ message: "User not found or no reviews" });
       }
-  })
-  .catch(err => {
+    })
+    .catch(err => {
       console.error("Error fetching reviews for user ID:", userId, err);
       res.status(500).json({ error: "Error fetching reviews", details: err });
-  });
+    });
 });
 
 app.post('/saveHotel', (req, res) => {
   const { userId, hotelId, hotelName } = req.body;
   UserModel.findByIdAndUpdate(
-      userId,
-      { $addToSet: { savedHotels: { hotelId, hotelName } } },
-      { new: true }
+    userId,
+    { $addToSet: { savedHotels: { hotelId, hotelName } } },
+    { new: true }
   )
-  .then(user => res.status(200).json(user.savedHotels))
-  .catch(err => res.status(500).json({ error: "Error saving hotel", details: err }));
+    .then(user => res.status(200).json(user.savedHotels))
+    .catch(err => res.status(500).json({ error: "Error saving hotel", details: err }));
 });
 
 
@@ -106,12 +108,12 @@ app.post('/saveHotel', (req, res) => {
 app.post('/unsaveHotel', (req, res) => {
   const { userId, hotelId } = req.body;
   UserModel.findByIdAndUpdate(
-      userId,
-      { $pull: { savedHotels: { hotelId } } }, // Depending on your Mongoose version, you might need to adjust this query
-      { new: true }
+    userId,
+    { $pull: { savedHotels: { hotelId } } }, // Depending on your Mongoose version, you might need to adjust this query
+    { new: true }
   )
-  .then(user => res.status(200).json(user.savedHotels))
-  .catch(err => res.status(500).json({ error: "Error unsaving hotel", details: err }));
+    .then(user => res.status(200).json(user.savedHotels))
+    .catch(err => res.status(500).json({ error: "Error unsaving hotel", details: err }));
 });
 
 
@@ -120,11 +122,11 @@ app.get('/api/hotelDetails/:hotelId', async (req, res) => {
   console.log("Fetching details for hotel ID:", hotelId);
   try {
     const response = await axios.get(`/api/hotelDetails?hotel_id=${hotelId}`);
-      console.log("Hotel details fetched successfully:", response.data);
-      res.json(response.data);
+    console.log("Hotel details fetched successfully:", response.data);
+    res.json(response.data);
   } catch (error) {
-      console.error('Failed to fetch hotel details:', error);
-      res.status(404).json({message: "Hotel details not found"});
+    console.error('Failed to fetch hotel details:', error);
+    res.status(404).json({ message: "Hotel details not found" });
   }
 });
 
