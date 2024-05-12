67eb1f7b3290728ca52fa83cf7504de0d1d05b2c
BeChoo <bcho2014@gmail.com>
2024-05-12 14:43:18 -0700

    Trying to fix CORS issue

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { UserModel, ReviewModel } = require('./models/User');
 const bcrypt = require('bcrypt');
 
 const corsOptions = {
-  origin: 'https://gotel-frontend.vercel.app', // http://localhost:3000
+  origin: 'https://gotel-frontend-git-main-bechoos-projects.vercel.app', // http://localhost:3000
   // METHODS: ["POST", "GET"],
   credentials: true,
   optionsSuccessStatus: 200
@@ -26,15 +26,15 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-  app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    UserModel.findOne({ email })
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email })
     .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-            res.json({ message: "Success", user });
-        } else {
-            res.status(401).json({ message: "Invalid credentials" });
-        }
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.json({ message: "Success", user });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
     })
     .catch(err => {
       console.error("Login error:", err);
@@ -74,12 +74,12 @@ app.post('/submitReview', (req, res) => {
 app.post('/updateProfilePic', (req, res) => {
   const { userId, profilePic } = req.body;
   UserModel.findByIdAndUpdate(
-      userId,
-      { $set: { profilePic: profilePic } },
-      { new: true }
+    userId,
+    { $set: { profilePic: profilePic } },
+    { new: true }
   )
-  .then(user => res.status(200).json({ message: "Profile picture updated", profilePic: user.profilePic }))
-  .catch(err => res.status(500).json({ error: "Error updating profile picture", details: err }));
+    .then(user => res.status(200).json({ message: "Profile picture updated", profilePic: user.profilePic }))
+    .catch(err => res.status(500).json({ error: "Error updating profile picture", details: err }));
 });
 
 // Get reviews by a user
@@ -129,7 +129,7 @@ app.get('/api/hotelDetails/:hotelId', async (req, res) => {
   const { hotelId } = req.params;
   console.log("Fetching details for hotel ID:", hotelId);
   try {
-    const response = await axios.get(`https://gotel-frontend.vercel.app/hotelDetails?hotel_id=${hotelId}`);
+    const response = await axios.get(`https://gotel-frontend-git-main-bechoos-projects.vercel.app/hotelDetails?hotel_id=${hotelId}`);
     console.log("Hotel details fetched successfully:", response.data);
     res.json(response.data);
   } catch (error) {
