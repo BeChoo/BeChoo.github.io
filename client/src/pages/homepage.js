c2c12e9ee7733b9d24f18d102c6327f5867fc027
BeChoo <bcho2014@gmail.com>
2024-05-13 12:48:19 -0700

    Hardcoded getCity

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -41,10 +41,12 @@ export default function Home() {
     //Function to fetch city data
     const getCity = async () => {
         try {
-            const res = await axios.get('https://gotel-api-gotel.vercel.app/api/city', {
-                params: { searchCity }
-            });
+            // const res = await axios.get('https://gotel-api-gotel.vercel.app/api/city?searchCity=paris', {
+            //     params: { searchCity }
+            // });
+            const res = await axios.get('https://gotel-api-gotel.vercel.app/api/city?searchCity=paris');
             const { data } = res;
+            console.log(res);
             setCity(data.data[0].gaiaId); //Set the city ID using API get request
         } catch (error) {
             console.log(error);
