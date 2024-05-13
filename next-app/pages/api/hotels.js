5d65fb4f3f0f56b62016c89307e685280c59793e
BeChoo <bcho2014@gmail.com>
2024-05-13 15:32:58 -0700

    CORS

--- next-app/pages/api/hotels.js
+++ next-app/pages/api/hotels.js
@@ -8,18 +8,33 @@ const cors = Cors({
 });
 
 //Function responsible for handling requests coming from API
-export default async function handler(_, res) {
+export default async function handler(req, res) {
   //RapidAPI endpoint configurations that hold all standard input options for gathering hotel data
   const options = {
     method: 'GET',
     url: 'https://hotels-com-provider.p.rapidapi.com/v2/hotels/search',
-   params: {
-      // query: req.query.searchCity,
+    params: {
+      region_id: req.query.city,
+      locale: 'en_GB',
+      checkin_date: req.query.checkIn,
+      sort_order: 'REVIEW',
+      adults_number: req.query.guests,
       domain: 'AE',
-      locale: 'en_GB'
+      checkout_date: req.query.checkOut,
+      children_ages: '4,0,15',
+      lodging_type: 'HOTEL,HOSTEL,APART_HOTEL',
+      price_min: '10',
+      star_rating_ids: '3,4,5',
+      meal_plan: 'FREE_BREAKFAST',
+      page_number: '1',
+      price_max: '500',
+      amenities: 'WIFI,PARKING',
+      payment_type: 'PAY_LATER,FREE_CANCELLATION',
+      guest_rating_min: '8',
+      available_filter: 'SHOW_AVAILABLE_ONLY'
     },
-    //RapidAPI key and host authentification 
     headers: {
+      //RapidAPI key and host authentification 
       "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
       "x-rapidapi-key": '55be8b6ed7mshd007e3fe20ca075p134d06jsn19e62b391381'
     }
