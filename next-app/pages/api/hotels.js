95ee9dab62c355d41334116ad9dbd9b2e9a548de
BeChoo <bcho2014@gmail.com>
2024-05-13 15:47:51 -0700

    CORS

--- next-app/pages/api/hotels.js
+++ next-app/pages/api/hotels.js
@@ -14,25 +14,24 @@ export default async function handler(req, res) {
     method: 'GET',
     url: 'https://hotels-com-provider.p.rapidapi.com/v2/hotels/search',
     params: {
-
       region_id: req.query.city,
-      // locale: 'en_GB',
-      // checkin_date: req.query.checkIn,
-      // sort_order: 'REVIEW',
-      // adults_number: req.query.guests,
-      // domain: 'AE',
-      // checkout_date: req.query.checkOut,
-      // children_ages: '4,0,15',
-      // lodging_type: 'HOTEL,HOSTEL,APART_HOTEL',
-      // price_min: '10',
-      // star_rating_ids: '3,4,5',
-      // meal_plan: 'FREE_BREAKFAST',
-      // page_number: '1',
-      // price_max: '500',
-      // amenities: 'WIFI,PARKING',
-      // payment_type: 'PAY_LATER,FREE_CANCELLATION',
-      // guest_rating_min: '8',
-      // available_filter: 'SHOW_AVAILABLE_ONLY'
+      locale: 'en_GB',
+      checkin_date: req.query.checkIn,
+      sort_order: 'REVIEW',
+      adults_number: req.query.guests,
+      domain: 'AE',
+      checkout_date: req.query.checkOut,
+      children_ages: '4,0,15',
+      lodging_type: 'HOTEL,HOSTEL,APART_HOTEL',
+      price_min: '10',
+      star_rating_ids: '3,4,5',
+      meal_plan: 'FREE_BREAKFAST',
+      page_number: '1',
+      price_max: '500',
+      amenities: 'WIFI,PARKING',
+      payment_type: 'PAY_LATER,FREE_CANCELLATION',
+      guest_rating_min: '8',
+      available_filter: 'SHOW_AVAILABLE_ONLY'
     },
     headers: {
       //RapidAPI key and host authentification 
@@ -41,14 +40,17 @@ export default async function handler(req, res) {
     }
   };
 
+  console.log(req, res);
   try {
     //Sends http rqeuest with the above configuration
-    const response = await axios.request(options);
+    const response = await axios.request(req);
+    
     //Sends a successful response with the data received from API
     res.status(200).json(response.data);
   } catch (error) {
     //Error handling in the event the response is not successful 
     console.error(error);
+
     res.status(500).json({ error: "Error" });
   }
 }
