257c66091e6d96ab46cc7d942a5187024df11550
BeChoo <bcho2014@gmail.com>
2024-05-16 14:46:23 -0700

    center description

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -5,7 +5,7 @@ import GoBeach from "../images/Go Beach.png";
 const About = () => {
     return (
         <div className="about-container">
-            <h4>Welcome to Gotel! We are a dedicated and creative team of four students from California State University Long Beach. Our mission is to revolutionize your travel experience by providing a seamless and intuitive web app that simplifies every aspect of your journey. Whether you're planning a vacation, a business trip, or a spontaneous getaway, Gotel is here to make your travels stress-free and enjoyable.</h4>
+            <div className="description">Welcome to Gotel! We are a dedicated and creative team of four students from California State University Long Beach. Our mission is to revolutionize your travel experience by providing a seamless and intuitive web app that simplifies every aspect of your journey. Whether you're planning a vacation, a business trip, or a spontaneous getaway, Gotel is here to make your travels stress-free and enjoyable.</div>
             <h3>Feel free to reach out to us!</h3>
             <h4>Brian Bui: brian.bui02@student.csulb.edu</h4>
             <h4>Brian Cho: brian.cho02@student.csulb.edu</h4>
